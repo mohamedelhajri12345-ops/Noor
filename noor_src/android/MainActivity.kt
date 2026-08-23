eb83b60a39c9898a213d19340c7339cb520496eb
Simo <mohamedelhajri12345@gmail.com>
2026-08-23 12:27:15 +0100

    Fix Android activity package

--- noor_src/android/MainActivity.kt
+++ noor_src/android/MainActivity.kt
@@ -1,17 +1,13 @@
-package com.comporx.noor
+package com.example.islamic
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val channelName = "noor/native"
-    private lateinit var bridge: NoorNativeBridge
-
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        bridge = NoorNativeBridge(this)
-        bridge.attach(MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName))
+        NoorNativeBridge(this).attach(MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName))
     }
 }
